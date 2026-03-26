c604734e536b5e638b4cffa0d344a2ab4fcd9c2e
Dorian Devois <dorian.devois@gmail.com>
2026-03-26 17:07:53 +0400

    add hbs and sass

---
+++ src/data/create-context.js
@@ -0,0 +1,22 @@
+import seo from './global/seo.json';
+import site from './global/site.json';
+import socials from './global/socials.json';
+
+import home from './pages/home.json';
+import athletes from './pages/athletes.json';
+import contacts from './pages/contacts.json';
+import news from './pages/news.json';
+
+const globalContext = {
+  baseUrl: process.env.NODE_ENV === 'production' ? '/vite-vituum-v2/' : '/',
+  seo,
+  site,
+  socials,
+
+  home,
+  contacts,
+  news,
+  athletes,
+};
+
+export { globalContext };
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite';
 import vituum from 'vituum';
+import handlebars from '@vituum/vite-plugin-handlebars';
+import { handlebarsHelpers } from './src/js/helpers/handlebars/index.js';
+import { globalContext } from './src/data/create-context';
 
 export default defineConfig({
   // base: '/',
   base: '/vite-vituum-v2/',
-  plugins: [vituum()],
+  plugins: [
+    vituum(),
+    handlebars({
+      data: [],
+      // 👇 глобальні дані (доступні всюди в HBS)
+      globals: {
+        ...globalContext,
+      },
+      partials: {
+        directory: 'src/partials',
+        extname: false, // {{> head}} замість {{'path/to/head.hbs'}}
+      },
+      // 👇 ТУТ ДОДАЄМО HELPERS
+      helpers: handlebarsHelpers,
+    }),
+  ],
 });
